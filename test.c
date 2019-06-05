ff25cda44dbe3aac9a1d058b527f8c662ba7c190
Тимофей <tymcrit@gmail.com>
2019-06-05 19:23:37 +0300

    Произведён рефакторинг

--- test.c
+++ test.c
@@ -29,19 +29,17 @@ void test_append() {
     string s = string_new();
     string_from_cstr(&s, "Some string");
     
-    string t = string_new();
-    string_from_cstr(&t, " with some information");
     
-    string_append(&s, t);
+    string_append(&s, slice_from_cstr(" with some information"));
     
     assert(!memcmp(s.data, "Some string with some information", s.len));
     
+    string t = string_new();
     string_free(&t);
     string_append(&s, t);
     assert(!memcmp(s.data, "Some string with some information", s.len));
     
-    string_from_cstr(&t, "");
-    string_append(&s, t);
+    string_append(&s, slice_from_cstr(""));
     assert(!memcmp(s.data, "Some string with some information", s.len));    
     
     string_free(&s);
@@ -57,21 +55,16 @@ void test_push() {
     string_from_cstr(&s, "Hello");
     string_push(&s, ' ');
     
-    string t = string_new();
-    string_from_cstr(&t, "World!");
-    
-    string_append(&s, t);
+    string_append(&s, slice_from_cstr("World!"));
     
     assert(!memcmp(s.data, "Hello World!", s.len));
     
     string_free(&s);
-    string_free(&t);
 }
 
 void test_pop() {
-    string s = string_new();
+    string s = slice_from_cstr("Some string!");
     
-    string_from_cstr(&s, "Some string!");
     char c = string_pop(&s);
     
     assert(c == '!');
@@ -81,9 +74,7 @@ void test_pop() {
 }
 
 void test_slice() {
-    string s = string_new();
-    
-    string_from_cstr(&s, "Some string!");
+    string s = slice_from_cstr("Some string!");
     
     string t;
     
@@ -106,23 +97,17 @@ void test_slice() {
 }
 
 void test_insert() {
-    string s = string_new();
-    string_from_cstr(&s, "HellWorld!");
-    
-    string t = string_new();
-    string_from_cstr(&t, "o ");
-    
-    string_insert(&s, t, 3);
+    string s = slice_from_cstr("HellWorld!");
+
+    string_insert(&s, slice_from_cstr("o "), 3);
     
     assert(!memcmp(s.data, "Hello World!", s.len));
     
     string_free(&s);
-    string_free(&t);
 }
 
 void test_trim() {
-    string s = string_new();
-    string_from_cstr(&s, "    ");
+    string s = slice_from_cstr("    ");
 
     string_trim_left(&s);
     assert(s.len == 0);
@@ -148,8 +133,7 @@ void test_trim() {
 }
 
 void test_cmp() {
-    string s = string_new();
-    string_from_cstr(&s, "HELLO WORLD");
+    string s = slice_from_cstr("HELLO WORLD");
 
     assert(string_lt(string_slice(s, 0, s.len - 1), s));
     assert(string_gt(s, string_slice(s, 1, s.len)));
@@ -160,22 +144,15 @@ void test_cmp() {
 }
 
 void test_find() {
-    string s = string_new();
-    string_from_cstr(&s, "Hello, World!");
+    string s = slice_from_cstr("Hello, World!");
 
-    string t = string_new();
-    string_from_cstr(&t, "ll");
+    assert(string_find(s, slice_from_cstr("ll")) == 2);
 
-    assert(string_find(s, t) == 2);
+    assert(string_find(s, slice_from_cstr("o")) == 4);
 
-    string_from_cstr(&t, "o");
-    assert(string_find(s, t) == 4);
-
-    string_from_cstr(&t, "A");
-    assert(string_find(s, t) == s.len);
+    assert(string_find(s, slice_from_cstr("A")) == s.len);
 
     string_free(&s);
-    string_free(&t);
 }
 
 int main() {
